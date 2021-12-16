8b89e7c1bf9ae2f4719bfdbfbeb696f4498de83b
Mayu Suzumura <mayumurasuzu@gmail.com>
2021-12-15 23:32:18 -0500

    change client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -22,7 +22,7 @@ $(function () {
   <article class="tweet">
     <header class="article-header">
       <h2 class="name">${tweetData.user.name}</h2>
-      <h2 class="account">${tweetData.user.name}</h2>
+      <h2 class="account">${tweetData.user.handle}</h2>
     </header>
     <div class="tweet-body">
       <p><b>
@@ -41,22 +41,30 @@ $(function () {
     return $tweet;
   };
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+  const data = [
+    {
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
+      },
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
+      },
+      created_at: 1461116232227,
     },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
+    {
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
+      },
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
     },
-    created_at: 1461116232227,
-  };
+  ];
 
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(data);
 });
